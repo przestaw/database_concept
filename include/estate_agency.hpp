bbeacc0b3695a996e0421ee31a20818d947e4c40
przestaw <p.stawczyk@stud.elka.pw.edu.pl>
2018-03-24 14:08:27 +0100

    Add files via upload

---
+++ include/estate_agency.hpp
@@ -0,0 +1,40 @@
+//
+// Created by Przemek on 23.03.2018.
+//
+
+#ifndef DATABASE_CONCEPT_ESTATE_AGENCY_HPP
+#define DATABASE_CONCEPT_ESTATE_AGENCY_HPP
+
+#include "database.hpp"
+using std::string
+//#define House Record
+//#define Owner Record
+class Database;
+class Owner;
+class House;
+class Size;
+class Price;
+class Adress;
+
+
+
+class Owner
+	:public Record
+{
+private:
+
+public:
+
+};
+
+class House
+	:public Record
+{
+private:
+
+public:
+
+};
+
+
+#endif //DATABASE_CONCEPT_ESTATE_AGENCY_HPP
